c70c1f09a969788b8efed2c7c1966a8cdc3ea05c
mistaOba <abdulhafeez.ola@gmail.com>
2020-01-28 09:18:08 +0000

    lol

---
+++ start/client/src/index.tsx
@@ -0,0 +1,54 @@
+import gql from 'graphql-tag';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider, useQuery } from '@apollo/react-hooks';
+import React from 'react';
+import ReactDOM from 'react-dom'; 
+import Pages from './pages';
+import Login from './pages/login';
+import injectStyles from './styles';
+import { resolvers, typeDefs } from './resolvers';
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+    const { data } = useQuery(IS_LOGGED_IN);
+    return data.isLoggedIn ? <Pages /> : <Login />;
+  }
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:4000/'
+});
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache,
+  link: new HttpLink({
+    uri: 'http://localhost:4000/graphql',
+    headers: {
+      authorization: localStorage.getItem('token'),
+    }, 
+  }),
+  typeDefs,
+  resolvers,
+});
+
+cache.writeData({
+    data: {
+      isLoggedIn: !!localStorage.getItem('token'),
+      cartItems: [],
+    },
+  });
+
+injectStyles();
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <IsLoggedIn />
+  </ApolloProvider>, 
+  document.getElementById('root')
+);
